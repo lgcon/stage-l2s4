95a08c984acf5acc8b98421c524f9b104d5d7c80
lgcon <lgcon@pinknet.u-strasbg.fr>
2016-06-01 16:45:41 +0200

    Adjusting comments and imports

--- src/bootstrap-lib/tabs.jsx
+++ src/bootstrap-lib/tabs.jsx
@@ -1,18 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {Add_host, Add_block} from '../forms/add.jsx';
-import {Translator, translate, updateTranslations} from '../lang.jsx';
+import {translate} from '../lang.jsx';
+
 
 
 
 /** 
  * Use this element to specify the different contents inside
- * a Tabs component (see below), the content must be put inside 
+ * a Tabs component (see below), the contents must be put inside 
  * a Pane. A Pane takes also a `label` property which names 
- * the current tab (note: this property is used by the Tabs component)
+ * the current tab.
+ * @note: this component just wrap his children inside a <div>
+ *	  his properties are used by the component Tabs.
  *
- * ex: <Pane label="Zoo"> <ListOfAnimals /> </Pane>
+ * example of use: 
+ *	<Pane label="Zoo"> <ListOfAnimals /> </Pane>
  */
+
 export var Pane = React.createClass({
 	
  	//contextTypes : {lang: React.PropTypes.string},
@@ -23,6 +27,12 @@ export var Pane = React.createClass({
 		);
 	}
 });
+
+
+
+
+
+
 
 /**
  * Use this component togheter with one or more Panes (see above)
@@ -44,15 +54,20 @@ export var Pane = React.createClass({
  * );
  *
  */
+
 export var Tabs = React.createClass({
 
 	/* Use context in order to update on language changes */
  	contextTypes : {lang: React.PropTypes.string},
 
+
+
 	/* The first tab is the one active by default */
 	getInitialState: function () {
 		return { selected: this.props.selected || 0 };
 	},
+
+
 
 	/* Update selected panel */
 	handleClick: function (index, event) {
@@ -61,17 +76,19 @@ export var Tabs = React.createClass({
 	},
 
 
+
 	/* Render the navigation bar */
 	_renderTitles: function () {
 
 		function labels(child, index) {
 
 			/* Label is active if selected */
-			var activeClass = this.state.selected === index ? 'active' : '' ;
+			var activeClass = (this.state.selected === index) ? 'active' : '' ;
 			
 			return ( 
 				<li key={"tlab"+index} className={activeClass}>
-					<a href = "#" onClick={this.handleClick.bind(this,index)}> 
+					<a href = "#" 
+					 onClick={this.handleClick.bind(this,index)}>
 						{translate(child.props.label)} 
 					</a> 
 				</li>
@@ -84,6 +101,9 @@ export var Tabs = React.createClass({
 			</ul>
 		);
 	},
+
+
+
 
 
 
@@ -106,17 +126,21 @@ export var Tabs = React.createClass({
 		
 		);
 
+
+
+
 		/**
 		 * Old version: renders only the selected contents
 		 * (note that the user-content on the other panels
 		 * will be erased as the component will be re-mounted)
+		 *
+		 *	return (
+		 *		<div className= "tabs-content" > 
+		 *		{this.props.children[this.state.selected]}
+		 *		</div>
+		 *	);
+		 *
 		 */
-
-	//	return (
-	//		<div className= "tabs-content" > 
-	//		{this.props.children[this.state.selected]}
-	//		</div>
-	//	);
 
 	},
 
